50b87c7ce32f5552c880e2b0fb155329432d8da8
Afaf Afridi <ibrahim.hasan00710@gmail.com>
2025-07-05 20:27:57 +0500

    game commit

---
+++ app.js
@@ -0,0 +1,9 @@
+let number=7
+let guess=prompt("Guess a number between 1 and 10");
+if(guess==number){
+    console.log("You guessed it right!");
+    alert("You guessed it right!");
+}else{
+    console.log("Try again!");
+    alert("Try again!");
+}
